601d492601c87aa3ecd39ca6dafc64262b063bfd
wpmenuka <azuremake60@gmail.com>
2025-10-04 00:18:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD~gR4GWTJI#jm2btUprSz6HkSSwdwzo_-ilMPZd6BLqQX0gAnd6rQ0'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?8voJgX'
 };
